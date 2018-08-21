07c3efb1b85eb4734ab74d494eba20cb627bda81
nelsondude <alexn1336@gmail.com>
2018-08-21 15:02:25 -0400

    Disable ssl require

--- tipsyapp/settings.py
+++ tipsyapp/settings.py
@@ -116,7 +116,7 @@ DATABASES = {
 }
 
 # Change 'default' database configuration with $DATABASE_URL.
-DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=False))
 
 
 
